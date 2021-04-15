42e8971d787d813e29d89179ad1ea10170a047e9
v8tenko <glebik8@gmail.com>
2021-04-16 00:04:20 +0300

    why?

--- src/Application.kt
+++ src/Application.kt
@@ -7,11 +7,42 @@ import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import java.text.DateFormat
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+// fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        install(CORS) {
+            header(HttpHeaders.AccessControlAllowOrigin)
+        }
+        install(ContentNegotiation) {
+            gson {
+                setDateFormat(DateFormat.LONG)
+                setPrettyPrinting()
+            }
+        }
+        routing {
+            post("/calc") {
+                val input = call.receive<ChemInput>()
+                val output = Solver.solve(
+                    input.n,
+                    input.a, input.b, input.c, input.d,
+                    input.h, input.s,
+                    input.t
+                )
+                call.respond(output)
+            }
+            get {
+                call.respondText("123")
+            }
+        }
+    }.start(wait = true)
 
-fun Application.module() {
+}
+
+/*fun Application.module() {
     install(CORS) {
         header(HttpHeaders.AccessControlAllowOrigin)
     }
@@ -36,7 +67,7 @@ fun Application.module() {
             call.respondText("123")
         }
     }
-}
+}*/
 
 data class ChemInput(
     val n: List<List<Double>>,
